09cf224fc14be2d3d4b4225684ff64c3da5a5257
Alok Kumar <alokkumaryadav.cse@gmail.com>
2024-03-30 21:57:28 +0530

    add backspace icon

--- calculator.js
+++ calculator.js
@@ -1,2 +1,11 @@
 let currentValue=``;
 document.querySelector('#display').value=currentValue;
+
+function checkInteger(currentValue) {
+    if (!Number.isInteger(currentValue)) {
+        currentValue = currentValue.toFixed(4);
+        console.log(`this is if block`);
+        console.log(currentValue);
+    }
+    else console.log(`this is else block`);
+}
